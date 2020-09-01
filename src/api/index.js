2390c16c3537713f369b527d19278905b5962581
Ali Arsalan Ansari <aliarslanansari@hotmail.com>
2020-09-01 23:48:19 +0530

    added song saga

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,20 @@
-const URL = 'https://itunes.apple.com/search?';
+import { create } from 'apisauce';
 const proxy = 'https://cors-anywhere.herokuapp.com/';
+const api = create({ baseURL: `${proxy}https://itunes.apple.com` });
+const URL = 'https://itunes.apple.com/search?';
 
 const fetchSongs = async search => {
+    search = search.replace(' ', '-');
     const response = await fetch(`${proxy}${URL}term=${search}&limit=48`);
     const data = await response.json();
     if (response.status >= 400) {
-        throw new Error(data.error);
+        throw new Error(response.error);
     }
     return data;
 };
-
 const fetchSong = async id => {
     console.log('FROM API SINGLE SONG', id);
-    const URL = `${proxy}http://itunes.apple.com/lookup?entity=song&id=${id}`;
+    const URL = `${proxy}https://itunes.apple.com/lookup?entity=song&id=${id}`;
     const response = await fetch(URL);
     const data = await response.json();
     if (response.status >= 400) {
@@ -21,3 +23,31 @@ const fetchSong = async id => {
     return data.results[0];
 };
 export { fetchSongs, fetchSong };
+
+const { ITUNES_URL } = process.env;
+
+const apiClients = {
+    itunes: null,
+    default: null
+};
+
+export const getApiClient = (type = 'itunes') => apiClients[type];
+
+export const generateApiClient = (type = 'github') => {
+    switch (type) {
+        case 'itunes':
+            apiClients[type] = createApiClientWithTransForm(ITUNES_URL);
+            return apiClients[type];
+        default:
+            apiClients.default = createApiClientWithTransForm(ITUNES_URL);
+            return apiClients.default;
+    }
+};
+
+export const createApiClientWithTransForm = baseURL => {
+    const api = create({
+        baseURL,
+        headers: { 'Content-Type': 'application/json' }
+    });
+    return api;
+};
